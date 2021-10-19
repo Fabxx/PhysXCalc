283b801b43b1c3adaa655cae824dfc5882e10c2a
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-20 00:02:17 +0200

    Update Pythagorean theorem.h

--- Physics Calculator/include/Pythagorean theorem.h
+++ Physics Calculator/include/Pythagorean theorem.h
@@ -10,21 +10,21 @@ public:
            
         static void powerenum1(structure &s) {
         
-            cout << "X axis" << endl;
-            cout << "Inser the number that you want to apply the power 1" << endl;
+            cout << "X axis, vector A" << endl;
+            cout << "Inser the number for A vector" << endl;
             cin >> s.x;
-            cout << "Insert the power to apply to the number 1" << endl;
-            cin >> s.y;
+            cout << "Applying power of 2 from theorem..." << endl;
+             s.y = 2;
             s.pow1 = pow(s.x, s.y);
         }
             
         static void powerenum2(structure &s) {
           
-            cout << "Y Axis" << endl;
-                cout << "Inser the number that you want to apply the power 2" << endl;
+            cout << "Y Axis vector B" << endl;
+                cout << "Inser the number for B vector" << endl;
                  cin >> s.z;
-                 cout << "Inser the number that you want to apply the power 2" << endl;
-                 cin >> s.k;
+                 cout << "Applying power of 2 from theorem..." << endl;
+                s.k = 2;
                  s.pow2 = pow(s.z, s.k);
         }
        
