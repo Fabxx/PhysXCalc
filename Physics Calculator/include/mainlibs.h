16b0f9a33cbab1ba859b77c852c97506fa9f7a72
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-23 15:22:01 +0200

    Delete Physics Calculator directory

--- Physics Calculator/include/mainlibs.h
+++
@@ -1,8 +0,0 @@
-#ifndef B0259D79_D35F_4B7E_85F5_9373FF68A1D0
-#define B0259D79_D35F_4B7E_85F5_9373FF68A1D0
-#include <iostream>
-#include <math.h>
-using namespace std;
-
-
-#endif /* B0259D79_D35F_4B7E_85F5_9373FF68A1D0 */
