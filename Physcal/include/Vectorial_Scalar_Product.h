045f373feb5169370210fec009f0f42e48ef83d3
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-26 23:51:46 +0200

    Add files via upload

---
+++ Physcal/include/Vectorial_Scalar_Product.h
@@ -0,0 +1,23 @@
+#ifndef F4D1F3C9_9761_4613_840F_DC06C9F6848C
+#define F4D1F3C9_9761_4613_840F_DC06C9F6848C
+#include <libs.h>
+
+class vett_product {
+    private:
+            static doubval vector_product;
+            static doubval scalar_product;
+    public:
+                static doubval product_store;
+                static doubval sinangle;
+                static doubval cosangle;
+                
+
+            static void muliply_modules();
+            static void cos_of_angle();
+            static void vectorial_product();
+            static void vectorial_scalar();
+            static void sin_of_angle();
+};
+
+
+#endif /* F4D1F3C9_9761_4613_840F_DC06C9F6848C */
