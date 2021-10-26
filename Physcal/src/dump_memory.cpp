08eb704ff3443cfb203fe0bc478c9bc0f8cb83b6
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-26 16:57:12 +0200

    Delete Physcal directory

--- Physcal/src/dump_memory.cpp
+++
@@ -1,9 +0,0 @@
-#include <libs.h>
-//This functions frees all the allocated memory for the vectors to work with new values.
-
-void memory::dump() {
-
-    angle::atanvett.clear();
-    single_module::moduleout.clear();
-    system("cls");
-}
