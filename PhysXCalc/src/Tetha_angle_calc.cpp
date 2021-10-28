9402afb631adbec1341c8f3c10b90430b941109c
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-28 12:06:47 +0200

    Update Tetha_angle_calc.cpp

--- PhysXCalc/src/Tetha_angle_calc.cpp
+++ PhysXCalc/src/Tetha_angle_calc.cpp
@@ -4,12 +4,12 @@
     double_t angle::tetha = 0;
     vett angle::atanvett;
 
-/*we calculate the atan of requested vectors where calculated the modules.
+/*we calculate the atan of requested vectors.
 if we calculated the modules of 2 vectors, we'll calculate 2 atans.
 The first module is stored in the i position, the second module is stored in the
 i+1 position.
 the atan is calculated on the initial vector coordinates. 
- Y coordinate / X coordinate in vectors.*/
+ Y coordinate[i] / X coordinate[i+1] in vectors.*/
 void angle::atan_components() {
     
     for (size_t i = 0; i < single_module::XYvett.size()-1; i+=2) {
@@ -25,6 +25,7 @@ void angle::atan_components() {
 angle between ALL the vectors, then we store the tetha result
 Obtained with all the vectors. it will be ONE tetha angle covering
 all the vectors putted. */
+
 void angle::tetha_angle() { 
         //change the order to not get negative value. 
            for (size_t i = 0; i < atanvett.size()-1; i+=2) {
