0ce68d192019d8af6c40814d338f4889d1ee7004
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-11-08 10:31:19 +0100

    Add files via upload

---
+++ PhysXCalc/include/Tetha_angle_calc.h
@@ -0,0 +1,15 @@
+#ifndef CBBDEF16_23B6_41AD_8497_080C174DCE6D
+#define CBBDEF16_23B6_41AD_8497_080C174DCE6D
+
+
+class angle { 
+        
+    public:
+             static double_t tetha;
+             static vett atanvett;
+             static void atan_components();
+             static void tetha_angle();
+
+};
+
+#endif /* CBBDEF16_23B6_41AD_8497_080C174DCE6D */
