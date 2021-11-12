8dfe96b8b030723614b27fef7bc9af5d444606c7
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-11-12 22:59:31 +0100

    Add files via upload

---
+++ PhysXCalc/include/delay.h
@@ -0,0 +1,12 @@
+#ifndef B4683234_7144_448B_A989_F69B9640084F
+#define B4683234_7144_448B_A989_F69B9640084F
+#include <libs.h>
+class delay {
+    private:
+            static clock_t start_time;
+    
+    public:
+            static void delaytime(int16_t milliseconds);
+};
+
+#endif /* B4683234_7144_448B_A989_F69B9640084F */
---
+++ PhysXCalc/src/delay.cpp
@@ -0,0 +1,12 @@
+#include <libs.h>
+
+clock_t delay::start_time;
+
+
+void delay::delaytime(int16_t milliseconds) {
+
+    milliseconds = milliseconds * 1000;
+    start_time = clock();
+    while (clock() < start_time + milliseconds);
+    
+}
