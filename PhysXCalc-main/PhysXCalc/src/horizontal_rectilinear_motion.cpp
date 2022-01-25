024250206b049844f125d70acc7f7aa2de26bb4b
-_ <30447649+Prox-y@users.noreply.github.com>
2022-01-25 15:36:55 +0100

    Delete PhysXCalc-main directory

--- PhysXCalc-main/PhysXCalc/src/horizontal_rectilinear_motion.cpp
+++
@@ -1,8 +0,0 @@
-#include <horizontal_rectilinear_motion.h>
-
-double_t rectilinear_motion::speed, rectilinear_motion::time;
-
-void rectilinear_motion::horizontal_motion() {
-
-        cout << "Insert speed value" << endl;
-}
