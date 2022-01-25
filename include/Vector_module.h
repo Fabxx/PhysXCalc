4ff32d1484432b858eb79086edb64d1c1ce018f5
-_ <30447649+Prox-y@users.noreply.github.com>
2022-01-25 15:37:37 +0100

    Add files via upload

---
+++ include/Vector_module.h
@@ -0,0 +1,21 @@
+#ifndef DFEBDC11_D1C2_49F9_AEF6_F8EA5BAA0B3E
+#define DFEBDC11_D1C2_49F9_AEF6_F8EA5BAA0B3E
+#include <stdlibs.h>
+#include <math.h>
+#include <vector>
+
+class modulefunc {
+        
+    public:
+            using vett = vector<double_t>;
+            static vett XYvett;
+            static vett modules;
+            static double_t XYcoord;
+            static bool operation;
+            static void coordinates_input();
+            static void module_calc();
+            static void module_output();
+
+};
+
+#endif /* DFEBDC11_D1C2_49F9_AEF6_F8EA5BAA0B3E */
---
+++ include/menu.h
@@ -0,0 +1,19 @@
+#ifndef D1E50C8C_2EB2_44D3_BB47_162D340D6639
+#define D1E50C8C_2EB2_44D3_BB47_162D340D6639
+#include <stdlibs.h>
+
+class graphics {
+    public:
+         static int16_t choice;
+
+        static void menu_init();
+        static void interface();
+};
+
+
+
+   
+
+
+
+#endif /* D1E50C8C_2EB2_44D3_BB47_162D340D6639 */
