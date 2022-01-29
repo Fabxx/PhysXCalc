4580d18612c0b20379a9c613119489ab4b8c0c99
revix <30447649+revix-0@users.noreply.github.com>
2022-01-29 15:49:40 +0100

    Update Vector_operations.cpp

--- physxcalc/src/Vector_operations.cpp
+++ physxcalc/src/Vector_operations.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 vector<double_t> modulefunc::modules, modulefunc::XYvett, angle::atanvett; 
-bool modulefunc::operation; 
+bool modulefunc::operation = false; 
 double_t angle::tetha = 0, modulefunc::XYcoord, vett_product::cosangle, vett_product::sinangle, vett_product::scalar_product, vett_product::product_store, 
 vett_product::vector_product;
 
@@ -12,7 +12,7 @@ Vector module section
 */
 
 void modulefunc::coordinates_input() {
-    operation = false;
+  
     cout << "Insert Vector coordinates, each 2 coordinates are a single vector" << endl
          << "Insert 1 to exit" << endl
          << "\033[0;31mNote: at least 4 coordinates must be inserted to proceed in the tetha calculation" << endl 
@@ -20,32 +20,25 @@ void modulefunc::coordinates_input() {
     
     /*size+1 so 0 < 1 is true and we start the input.
     in this case we have to tell him when we want to stop
-    if we choose to exit and there are at least even numbers of coords
-    or there are no coords at all we return to menu.
-    This protection avoids segmentation fault by aborting next calculations.*/
+    if we choose to exit and there are at least even numbers of coords >= 4
+    in all other cases, if it's empty or odd, we return to menu.
+    This protection avoids segmentation fault by aborting next calculations.
+    we can't calculate the tetha angle if we obtain only one atan with one vector.
+    At least two atans must be there to calculate the tetha.*/
     
     for (size_t i = 0; i < XYvett.size()+1; i++){    
             cin >> XYcoord;
-            /*Size must be even and at least 4 to represent 2 vectors or else
-            we can't calculate the tetha angle if we obtain only one atan with one vector.
-            At least two atans must be there to calculate the tetha.*/
             if ((XYcoord == 1 && XYvett.size()%2 == 0) && (XYvett.size() >= 4)) {
                 i = XYvett.size();
-            
             } else if ((XYcoord == 1 && XYvett.size()%2!= 0)) {
-
                 cout << "\033[0;31mAt least even numbers of coordinates must be inserted." << endl;
                 operation = true;
-
              } else {
                 XYvett.push_back(XYcoord);   
         }    
     }   
 }
 
-
-//calculating module for each vector.
-//checking if the vector is not empty, doing it here to avoid conflict with first IF, since 0 is even.
 void modulefunc::module_calc() {
              for (size_t i = 0; i < XYvett.size()-1; i+=2) { 
             modules.push_back(sqrt((XYvett[i] * XYvett[i]) + (XYvett[i+1] * XYvett[i+1])));
@@ -64,14 +57,12 @@ for (size_t i = 0; i < modules.size(); i++){
 /*###############################
 Vector atan section
   ###############################
-*/
-
-/*we calculate the atan of requested vectors.
-if we calculated the modules of 2 vectors, we'll calculate 2 atans.
-The first module is stored in the i position, the second module is stored in the
-i+1 position.
+we calculate the atan of requested vectors.
 the atan is calculated on the initial vector coordinates. 
- Y coordinate[i+1] / X coordinate[i] in vectors.*/
+ Y coordinate[i+1] / X coordinate[i] in vectors. Then we skip to the 
+ next 2 coordinates.
+ */
+
 void angle::atan_calculation() {
     
     for (size_t i = 0; i < modulefunc::XYvett.size()-1; i+=2) {
@@ -83,9 +74,7 @@ void angle::atan_calculation() {
       
 }       
 
-/*we get all the atans for N vectors to calculate the total
-angle between ALL the vectors, then we store the tetha result
-Obtained with all the vectors. it will be ONE tetha angle covering
+/*we store the tetha result Obtained with all the vectors. it will be ONE tetha angle covering
 all the vectors putted. */
 
 void angle::tetha_angle() { 
@@ -104,6 +93,7 @@ void angle::tetha_angle() {
 /*###############################
 Vector product section
   ###############################
+  Calculating SIN, COS, vectorial product and vectorial scalar product.
 */
 
 
